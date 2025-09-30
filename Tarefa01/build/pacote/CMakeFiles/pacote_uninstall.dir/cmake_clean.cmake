9a144052b8631fe6f8109c68d225bc6c1526b89c
Letizia L. Baptistella <letizia.baptistella@gmail.com>
2025-09-30 13:46:05 -0300

    tarefa atualizada

---
+++ Tarefa01/build/pacote/CMakeFiles/pacote_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pacote_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/pacote_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
